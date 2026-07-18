4987c31d3d4efeeb95399456a562cbc33026b7db
Wisdom54 <wisdomkomlakumah@gmail.com>
2026-07-18 01:03:14 +0000

    Add files via upload

---
+++ QUESTION 12.CPP
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    for (int i = 1; i <= num; i++) {
+        if (i % 2 != 0) {
+            cout << i << " ";
+        }
+    }
+
+    return 0;
+}
